c31c455bd00f3df5faec29d1e67a7aec499dcc15
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-10-20 03:29:13 -0300

    Update the entity tree structure

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'drizzle-kit';
 
 export default {
-	schema: './src/entities/*.ts',
+	schema: './src/*/*.entity.ts',
 	out: './migrations',
 	dialect: 'postgresql',
 	dbCredentials: {

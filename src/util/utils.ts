5315031f1912d3235e9f4cecf560185b7e7df9ba
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-10-20 03:29:43 -0300

    Create utils.ts

---
+++ src/util/utils.ts
@@ -0,0 +1,87 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiQuery } from '@nestjs/swagger';
+
+export const takeUniqueOrThrow = <T extends any[]>(values: T): T[number] => {
+	if (values.length !== 1)
+		throw new Error('Found non-unique or inexistent value');
+	return values[0]!;
+};
+
+export const takeUniqueOrNull = <T extends any[]>(
+	values: T,
+): T[number] | null => {
+	if (values.length > 1) return null;
+	return values[0]!;
+};
+
+export const takeUniqueOrAct = <T extends any[]>(
+	values: T,
+	callback: () => void,
+): T[number] => {
+	if (values.length !== 1) callback();
+	return values[0]!;
+};
+
+export function ApiPaged() {
+	return applyDecorators(
+		ApiQuery({
+			name: 'page',
+			required: false,
+			type: Number,
+			description: 'Page number',
+		}),
+		ApiQuery({
+			name: 'pageSize',
+			required: false,
+			type: Number,
+			description: 'Number of items per page',
+		}),
+		ApiQuery({
+			name: 'filter',
+			required: false,
+			type: String,
+			description: 'Filter string',
+		}),
+	);
+}
+
+export interface GoodResponse<T> {
+	status: 'success';
+	data: T | null;
+}
+
+export interface BadResponse {
+	status: 'error';
+	message: string | null;
+	url: string;
+}
+
+export type StandardResponse<T> = GoodResponse<T> | BadResponse;
+
+export function success<T>(data?: T | null): GoodResponse<T> {
+	return {
+		status: 'success',
+		data: data ?? null,
+	};
+}
+
+export function failure(message: string | undefined, url: string): BadResponse {
+	return {
+		status: 'error',
+		message: message ?? null,
+		url,
+	};
+}
+
+export interface PagedResult<T> {
+	data: T;
+	total: number;
+	page: number;
+	pageSize: number;
+}
+export interface PagedResult<T> {
+	data: T;
+	total: number;
+	page: number;
+	pageSize: number;
+}

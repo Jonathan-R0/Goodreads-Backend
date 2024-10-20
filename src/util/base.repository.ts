ed2490c2438ffdf7dbf050e9076db365216d7e72
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-10-20 03:30:38 -0300

    Craete base repositories and services

---
+++ src/util/base.repository.ts
@@ -0,0 +1,127 @@
+import { and, eq } from 'drizzle-orm/expressions';
+import db from '@/config/db.config';
+import { takeUniqueOrAct, takeUniqueOrThrow } from '@/util/utils';
+import {
+	getTableColumns,
+	InferInsertModel,
+	InferSelectModel,
+	SQLWrapper,
+} from 'drizzle-orm';
+import { PgColumn, PgTable } from 'drizzle-orm/pg-core';
+import { PagedResult } from '@/util/utils';
+
+type EntityReturn<TableType extends PgTable> = InferSelectModel<TableType>;
+type Entity<TableType extends PgTable> = InferInsertModel<TableType>;
+type PrimaryKey<TableType extends PgTable> =
+	InferSelectModel<TableType>[PgColumn['name']];
+
+export class BaseRepository<TableType extends PgTable> {
+	protected readonly table: TableType;
+	protected readonly idColumn: PgColumn;
+
+	constructor(table: TableType, idColumn: PgColumn) {
+		this.table = table;
+		this.idColumn = idColumn;
+	}
+
+	public async exists(id: PrimaryKey<TableType>): Promise<boolean> {
+		return await db
+			.select()
+			.from(this.table)
+			.where(eq(this.idColumn, id))
+			.then((result) => result.length > 0);
+	}
+
+	public async findById(
+		id: PrimaryKey<TableType>,
+		callbackWhenNotFound: () => void = () => {
+			throw new Error('Found non-unique or inexistent value');
+		},
+	): Promise<EntityReturn<TableType>> {
+		return await db
+			.select()
+			.from(this.table)
+			.where(eq(this.idColumn, id))
+			.then(<T extends any[]>(values: T): T[number] =>
+				takeUniqueOrAct(values, callbackWhenNotFound),
+			);
+	}
+
+	public async findAllWhere(
+		andsConditions: (SQLWrapper | undefined)[] = [],
+		page?: number,
+		pageSize?: number,
+	): Promise<PagedResult<EntityReturn<TableType>[]>> {
+		const partialResp = db
+			.select()
+			.from(this.table)
+			.where(and(...andsConditions));
+		const resp = await (page && pageSize
+			? partialResp.limit(pageSize).offset((page - 1) * pageSize)
+			: partialResp);
+		return {
+			data: resp,
+			total: resp.length,
+			page: page || 0,
+			pageSize: pageSize || 0,
+		};
+	}
+
+	public async create(
+		entity: Entity<TableType>,
+	): Promise<EntityReturn<TableType>> {
+		const columns = this.getColumns(this.table);
+		return await db
+			.insert(this.table)
+			.values({
+				...entity,
+				...(columns.has('updated_at')
+					? { updated_at: new Date() }
+					: {}),
+				...(columns.has('created_at')
+					? { created_at: new Date() }
+					: {}),
+			})
+			.returning()
+			.then(takeUniqueOrThrow);
+	}
+
+	public async update(
+		entity: Partial<Entity<TableType> & { id: PrimaryKey<TableType> }>,
+	): Promise<EntityReturn<TableType>> {
+		return this.updateById(entity, entity.id);
+	}
+
+	public async updateById(
+		entity: Partial<Entity<TableType>>,
+		id: PrimaryKey<TableType>,
+	): Promise<EntityReturn<TableType>> {
+		return await db
+			.update(this.table)
+			.set(
+				this.getColumns(this.table).has('updated_at')
+					? entity
+					: { ...entity, updated_at: new Date() },
+			)
+			.where(eq(this.idColumn, id))
+			.returning()
+			.then(takeUniqueOrThrow);
+	}
+
+	public async delete(
+		id: PrimaryKey<TableType>,
+	): Promise<EntityReturn<TableType>> {
+		return await db
+			.delete(this.table)
+			.where(eq(this.idColumn, id))
+			.returning()
+			.then(takeUniqueOrThrow);
+	}
+
+	private getColumns(table: TableType): Set<string> {
+		return Object.keys(getTableColumns(table)).reduce(
+			(acc, key) => acc.add(key),
+			new Set<string>(),
+		);
+	}
+}
---
+++ src/util/base.service.ts
@@ -0,0 +1,35 @@
+import { PgColumn, PgTable } from 'drizzle-orm/pg-core';
+import { BaseRepository } from './base.repository';
+import { InferSelectModel, InferInsertModel } from 'drizzle-orm';
+
+type EntityReturn<TableType extends PgTable> = InferSelectModel<TableType>;
+type Entity<TableType extends PgTable> = InferInsertModel<TableType>;
+type PrimaryKey<TableType extends PgTable> =
+	InferSelectModel<TableType>[PgColumn['name']];
+
+export class BaseService<
+	TableType extends PgTable,
+	R extends BaseRepository<TableType>,
+> {
+	constructor(private readonly repository: R) {}
+
+	async create(entity: Entity<TableType>): Promise<EntityReturn<TableType>> {
+		return this.repository.create(entity);
+	}
+
+	async update(
+		entity: Partial<Entity<TableType> & { id: PrimaryKey<TableType> }>,
+	): Promise<EntityReturn<TableType>> {
+		return this.repository.update(entity);
+	}
+
+	async delete(id: PrimaryKey<TableType>): Promise<EntityReturn<TableType>> {
+		return this.repository.delete(id);
+	}
+
+	async findById(
+		id: PrimaryKey<TableType>,
+	): Promise<EntityReturn<TableType>> {
+		return this.repository.findById(id);
+	}
+}

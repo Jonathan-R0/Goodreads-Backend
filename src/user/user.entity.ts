bd2c2eb141b4a81441fbdf3864e12fcd4a562fe7
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-10-20 21:11:29 -0300

    Make user email be unique

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -4,7 +4,7 @@ import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 export const usersTable = pgTable('users', {
 	id: serial('id').primaryKey(),
 	name: varchar('name', { length: 256 }),
-	email: text('email'),
+	email: text('email').unique(),
 	password: text('password'),
 	role: text('role'),
 });

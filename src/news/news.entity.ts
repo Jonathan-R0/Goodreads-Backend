187821f35fe1a9cd614cc62e78e57a4f57f7b18e
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2024-11-12 18:29:50 -0300

    Add created at field on news entity

--- src/news/news.entity.ts
+++ src/news/news.entity.ts
@@ -1,12 +1,13 @@
 import usersTable from '@/user/user.entity';
 import { InferSelectModel } from 'drizzle-orm';
-import { integer, pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const newsTable = pgTable('news', {
 	id: serial('id').primaryKey(),
 	title: text('title'),
 	content: text('content'),
 	author_id: integer('author_id').references(() => usersTable.id),
+	created_at: timestamp('created_at').defaultNow(),
 });
 
 export default newsTable;
